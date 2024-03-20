230dea4aeb96194d5ecd2aa4af309b323d8ce565
Brian Ott <bott@b1-systems.de>
2024-03-20 16:00:23 +0100

    add: init functions

--- project.py
+++ project.py
@@ -2,10 +2,18 @@ import random
 
 
 class Player():
-    ...
+    def __init__(self, name):
+        self.name = name
+        self.score = 0
 
 
 class Game():
+    def __init__(self):
+        self.choices = ['rock', 'paper', 'scissors', 'lizard', 'spock']
+        self.player_name = Player(player_name)
+        self.computer_name = Player('computer')
+        self.num_rounds = num_rounds
+        self.round_number = round_number
     ...
 
 
