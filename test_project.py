045cdf3665e776942d84b4c9f3d30f649f9b45d9
Brian Ott <bott@b1-systems.de>
2024-04-06 22:02:45 +0200

    add: test function names

--- test_project.py
+++ test_project.py
@@ -1,1 +1,39 @@
 import pytest
+from project import Game
+from io import StringIO
+
+
+class TestGame:
+    ...
+
+
+    def test_valid_player_choice(self, monkeypatch):
+        ...
+
+
+    def test_initial_player_score(self):
+        ...
+
+
+    def test_round_number_increase(self, monkeypatch):
+        ...
+
+
+    def test_invalid_player_choice(self, monkeypatch):
+        ...
+
+
+    def test_game_end(self, monkeypatch):
+        ...
+
+
+    def test_display_player_choice(self, monkeypatch):
+        ...
+
+
+    def test_valid_computer_choice(self):
+        ...
+
+
+    def test_invalid_computer_choice(self):
+        ...
