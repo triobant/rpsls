203ad0398a15eea8e989010bc05bc8bc0578fe55
Brian Ott <bott@b1-systems.de>
2024-04-27 22:22:47 +0200

    add: class colors

--- project.py
+++ project.py
@@ -1,6 +1,17 @@
 import random
 
 
+class Colors:
+    RED = '\033[91m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    BLUE = '\033[94m'
+    MAGENTA = '\033[95m'
+    CYAN = '\033[96m'
+    WHITE = '\033[97m'
+    RESET = '\033[0m'
+
+
 class Player():
     def __init__(self, name):
         self.name = name
