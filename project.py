1f7f98955b66a300916b4a87061a1178ac72394b
Brian Ott <bott@b1-systems.de>
2024-05-04 22:27:53 +0200

    change: removed unnecessary colors

--- project.py
+++ project.py
@@ -6,8 +6,6 @@ class Colors:
     GREEN = '\033[92m'
     YELLOW = '\033[93m'
     BLUE = '\033[94m'
-    MAGENTA = '\033[95m'
-    CYAN = '\033[96m'
     WHITE = '\033[97m'
     RESET = '\033[0m'
 
