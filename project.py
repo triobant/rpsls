da70e86ecde4558c1a92eec98b623b8fcc3b4bd0
Brian Ott <bott@b1-systems.de>
2024-05-24 18:09:01 +0200

    change: format

--- project.py
+++ project.py
@@ -2,103 +2,94 @@ import random
 
 
 class Colors:
-    RED = '\033[91m'
-    GREEN = '\033[92m'
-    YELLOW = '\033[93m'
-    BLUE = '\033[94m'
-    RESET = '\033[0m'
+    RED = "\033[91m"
+    GREEN = "\033[92m"
+    YELLOW = "\033[93m"
+    BLUE = "\033[94m"
+    RESET = "\033[0m"
 
 
-class Player():
+class Player:
     def __init__(self, name):
         self.name = name
         self.score = 0
 
 
-class Game():
+class Game:
     def __init__(self, player_name, num_rounds):
-        self.choices = ['rock', 'paper', 'scissors', 'lizard', 'spock']
+        self.choices = ["rock", "paper", "scissors", "lizard", "spock"]
         self.player = Player(player_name)
-        self.computer = Player('Computer')
+        self.computer = Player("Computer")
         self.num_rounds = num_rounds
         self.round_number = 0
 
-
     def determine_winner(self, player_choice, computer_choice):
         rules = {
-                'rock': ['scissors', 'lizard'],
-                'paper': ['rock', 'spock'],
-                'scissors': ['paper', 'lizard'],
-                'lizard': ['paper', 'spock'],
-                'spock': ['rock', 'scissors']
+            "rock": ["scissors", "lizard"],
+            "paper": ["rock", "spock"],
+            "scissors": ["paper", "lizard"],
+            "lizard": ["paper", "spock"],
+            "spock": ["rock", "scissors"],
         }
 
-
         if player_choice == computer_choice:
-            return f'{Colors.YELLOW}Draw!{Colors.RESET}', 0
+            return f"{Colors.YELLOW}Draw!{Colors.RESET}", 0
         elif computer_choice in rules[player_choice]:
-            return f'{Colors.RED}You score!{Colors.RESET}', 1
+            return f"{Colors.RED}You score!{Colors.RESET}", 1
         else:
-            return f'{Colors.BLUE}Computer scores!{Colors.RESET}', -1
-
+            return f"{Colors.BLUE}Computer scores!{Colors.RESET}", -1
 
     def determine_final_winner(self):
         if self.player.score > self.computer.score:
-            return f'{Colors.RED}You are the champion!{Colors.RESET}'
+            return f"{Colors.RED}You are the champion!{Colors.RESET}"
         elif self.player.score < self.computer.score:
-            return f'{Colors.BLUE}Computer is the champion!{Colors.RESET}'
+            return f"{Colors.BLUE}Computer is the champion!{Colors.RESET}"
         else:
-            return f'{Colors.YELLOW}Draw?!{Colors.RESET}'
-
+            return f"{Colors.YELLOW}Draw?!{Colors.RESET}"
 
     def play_round(self):
         player_choice = self.get_player_choice()
         computer_choice = random.choice(self.choices)
-        print(f'\n{Colors.RED}You chose: {player_choice}{Colors.RESET}')
-        print(f'{Colors.BLUE}Computer chose: {computer_choice}{Colors.RESET}')
+        print(f"\n{Colors.RED}You chose: {player_choice}{Colors.RESET}")
+        print(f"{Colors.BLUE}Computer chose: {computer_choice}{Colors.RESET}")
         result, score = self.determine_winner(player_choice, computer_choice)
         print(result)
-
 
         if score == 1:
             self.player.score += 1
         elif score == -1:
             self.computer.score += 1
 
-
         self.round_number += 1
-        print(f'{Colors.RED}Your score: {self.player.score}{Colors.RESET}, {Colors.BLUE}Computer score: {self.computer.score}{Colors.RESET}\n')
-
+        print(
+            f"{Colors.RED}Your score: {self.player.score}{Colors.RESET}, {Colors.BLUE}Computer score: {self.computer.score}{Colors.RESET}\n"
+        )
 
     def get_player_choice(self):
         while True:
-            print(f'\n{Colors.RED}Choose your move!{Colors.RESET}\n')
+            print(f"\n{Colors.RED}Choose your move!{Colors.RESET}\n")
             for i, choice in enumerate(self.choices, start=1):
-                print(f'{i}. {choice}')
-            print(f'0. Quit\n6. Read rules\n')
-            choice_index = input('Enter your choice (1-6): ')
+                print(f"{i}. {choice}")
+            print(f"0. Quit\n6. Read rules\n")
+            choice_index = input("Enter your choice (1-6): ")
 
-
-            if choice_index == '0':
-                print(f'\n{Colors.GREEN}Thanks for playing!{Color.RESET}')
+            if choice_index == "0":
+                print(f"\n{Colors.GREEN}Thanks for playing!{Color.RESET}")
                 exit()
 
-
-            if choice_index == '6':
+            if choice_index == "6":
                 self.display_rules()
                 continue
 
-
             if choice_index.isdigit() and 1 <= int(choice_index) <= 5:
                 return self.choices[int(choice_index) - 1]
-            elif choice_index == '':
+            elif choice_index == "":
                 return random.choice(self.choices)
             else:
-                print('Invalid choice. Please choose a number between 1 and 6')
-
+                print("Invalid choice. Please choose a number between 1 and 6")
 
     def display_rules(self):
-        rules = '''Scissors cuts paper,
+        rules = """Scissors cuts paper,
 Paper covers rock,
 Rock crushes lizard,
 Lizard poisons Spock,
@@ -108,23 +99,25 @@ Lizard eats paper,
 Paper disproves Spock,
 Spock vaporizes rock,
 and as it always has,
-Rock crushes scissors.'''
-        print(f'\nRules of the Game: {rules}')
+Rock crushes scissors."""
+        print(f"\nRules of the Game: {rules}")
 
 
 def main():
-    player_name = str(input('Player! Please enter your name: ').strip())
-    if player_name == '':
-        player_name = 'User'
-    num_rounds = input(f'Welcome {Colors.RED}{player_name}{Colors.RESET}!\nPlease enter the amount of rounds you want to play: ')
-    if num_rounds == '':
+    player_name = str(input("Player! Please enter your name: ").strip())
+    if player_name == "":
+        player_name = "User"
+    num_rounds = input(
+        f"Welcome {Colors.RED}{player_name}{Colors.RESET}!\nPlease enter the amount of rounds you want to play: "
+    )
+    if num_rounds == "":
         num_rounds = 3
-        print(f'Default round number: {num_rounds}')
+        print(f"Default round number: {num_rounds}")
     game = Game(player_name, int(num_rounds))
     while game.round_number < game.num_rounds:
         game.play_round()
     print(game.determine_final_winner())
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- test_project.py
+++ test_project.py
@@ -5,64 +5,55 @@ from io import StringIO
 
 class TestGame:
 
-
     def test_valid_player_choice(self, monkeypatch):
-        game = Game('Test Valid Player Choice', 1)
-        monkeypatch.setattr('sys.stdin', StringIO('1\n'))
+        game = Game("Test Valid Player Choice", 1)
+        monkeypatch.setattr("sys.stdin", StringIO("1\n"))
         player_choice = game.get_player_choice()
         assert player_choice in game.choices
 
-
     def test_initial_player_score(self):
-        game = Game('Test Player Score', 1)
+        game = Game("Test Player Score", 1)
         assert game.player.score == 0
 
-
     def test_round_number_increase(self, monkeypatch):
-        game = Game('Test Round Number Increase', 1)
-        monkeypatch.setattr('sys.stdin', StringIO('1\n'))
+        game = Game("Test Round Number Increase", 1)
+        monkeypatch.setattr("sys.stdin", StringIO("1\n"))
         game.round_number = 0
         game.play_round()
         assert game.round_number == 1
 
-
     def test_invalid_player_choice(self, monkeypatch):
-        game = Game('Test Invalid Player Choice', 1)
-        monkeypatch.setattr('sys.stdin', StringIO('1\n'))
+        game = Game("Test Invalid Player Choice", 1)
+        monkeypatch.setattr("sys.stdin", StringIO("1\n"))
         with pytest.raises(ValueError):
-            game.get_player_choice('Invalid choice')
-
+            game.get_player_choice("Invalid choice")
 
     def test_game_end(self, monkeypatch):
-        game = Game('Test Game End', 1)
-        monkeypatch.setattr('sys.stdin', StringIO('1\n'))
+        game = Game("Test Game End", 1)
+        monkeypatch.setattr("sys.stdin", StringIO("1\n"))
         game.round_number = 3
         assert game.round_number == game.num_rounds
 
-
     def test_display_player_choice(self, monkeypatch):
-        game = Game('Test Display Player Choice', 1)
-        monkeypatch.setattr('sys.stdin', StringIO('1\n'))
-        assert game.get_player_choice('Enter your choice (1-6): ') == game.choices[0]
-
+        game = Game("Test Display Player Choice", 1)
+        monkeypatch.setattr("sys.stdin", StringIO("1\n"))
+        assert game.get_player_choice("Enter your choice (1-6): ") == game.choices[0]
 
     def test_valid_computer_choice(self):
-        game = Game('Test Valid Computer choice', 1)
+        game = Game("Test Valid Computer choice", 1)
         computer_choice = game.computer_choice()
         assert computer_choice in game.choices
 
-
     def test_invalid_computer_choice(self):
-        game = Game('Test Invalid Computer choice', 1)
-        computer_choice = 'invalid'
+        game = Game("Test Invalid Computer choice", 1)
+        computer_choice = "invalid"
         assert computer_choice not in game.choices
         with pytest.raises(ValueError):
-            game.computer_choice('invalid choice')
-
+            game.computer_choice("invalid choice")
 
     def test_determine_final_winner(self, monkeypatch):
-        game = Game('Test Final Winner', 1)
-        monkeypatch.setattr('sys.stdin', StringIO('1\n'))
+        game = Game("Test Final Winner", 1)
+        monkeypatch.setattr("sys.stdin", StringIO("1\n"))
         game.player.score = 2
         game.computer.score = 1
         assert game.player.score > game.computer.score
